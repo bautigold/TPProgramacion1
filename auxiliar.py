032a78e457427583e85764eb7ff5d182d909c8b3
Bauti <bautigoldberg@gmail.com>
2026-05-28 13:46:56 -0300

    agrego funciones auxiliares

--- auxiliar.py
+++ auxiliar.py
@@ -1,8 +1,12 @@
-import re 
+import re
 
 def validar_dni(dni):
-    """
-    Objetivo: Validar que el DNI tenga 7 u 8 dígitos numéricos
-    """
-    patron = r"^\d{7,8}$" 
+    patron = r"^\d{7,8}$"
     return bool(re.match(patron, dni))
+
+def validar_codigo_viaje(codigo):
+    patron = r"^[A-Z]{2}\d{3}$"
+    return bool(re.match(patron, codigo))
+
+def normalizar_texto(texto):
+    return texto.strip().title()
