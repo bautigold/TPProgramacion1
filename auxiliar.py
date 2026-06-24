127326115f21b6742d1f0fa1d582ca8285fb1ae9
Jan <janmodzelewski2006@gmail.com>
2026-06-23 22:24:16 -0300

    Actualizo auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -1,4 +1,6 @@
 import re
+import json
+import os
 
 def validar_dni(dni):
     patron = r"^\d{7,8}$"
@@ -10,3 +12,38 @@ def validar_codigo_viaje(codigo):
 
 def normalizar_texto(texto):
     return texto.strip().title()
+
+def obtener_ruta(nombre_archivo):
+    ruta_actual = os.path.dirname(__file__)
+    return os.path.join(ruta_actual, nombre_archivo)
+
+def guardar_datos(datos, nombre_archivo):
+    ruta = obtener_ruta(nombre_archivo)
+    try:
+        with open(ruta, "w") as archivo:
+            json.dump(datos, archivo, indent=4)
+    except Exception as e:
+        print(f"Ocurrió un error al guardar: {e}")
+
+def cargar_datos(nombre_archivo, estructura_defecto):
+    ruta = obtener_ruta(nombre_archivo)
+    try:
+        with open(ruta, "r") as archivo:
+            return json.load(archivo)
+    except Exception as e:
+        return estructura_defecto
+
+def leer_entero(mensaje):
+    while True:
+        try:
+            return int(input(mensaje))
+        except Exception as e:
+            print(f"Entrada inválida ({e}). Intente de nuevo.")
+
+def leer_float(mensaje):
+    while True:
+        try:
+            return float(input(mensaje))
+        except Exception as e:
+            print(f"Entrada inválida ({e}). Ingrese un número decimal.")
+
